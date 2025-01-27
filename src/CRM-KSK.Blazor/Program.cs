2863a8686e2437d3939cc5fd8fd389e5847895d1
Dmitriy Bilitsa <dbilica19@gmail.com>
2025-01-27 14:37:15 +0300

    fix

--- src/CRM-KSK.Blazor/Program.cs
+++ src/CRM-KSK.Blazor/Program.cs
@@ -1,4 +1,5 @@
 using CRM_KSK.Blazor;
+using CRM_KSK.Blazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7268") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7268/api") });
+builder.Services.AddScoped<ClientService>();
 
 await builder.Build().RunAsync();
