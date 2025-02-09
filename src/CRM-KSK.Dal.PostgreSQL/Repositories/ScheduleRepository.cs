232fbfb67c566dc3f06a933b4c78293a698abab0
Dmitriy Bilitsa <dbilica19@gmail.com>
2025-02-09 21:14:43 +0300

    сделал нормально отображение имён в расписании, обновление записи в расписании, чуть чуть настроил стили для таблицы

--- src/CRM-KSK.Dal.PostgreSQL/Repositories/ScheduleRepository.cs
+++ src/CRM-KSK.Dal.PostgreSQL/Repositories/ScheduleRepository.cs
@@ -26,7 +26,75 @@ public class ScheduleRepository : IScheduleRepository
 
     public async Task AddOrUpdateSchedule(Schedule schedule, CancellationToken cancellationToken)
     {
-        _context.Schedules.Add(schedule);
+        var existingSchedule = await _context.Schedules
+            .Include(c => c.Clients)
+            .Include(t => t.Trainer)
+            .FirstOrDefaultAsync(s => s.Id == schedule.Id, cancellationToken);
+
+        if(existingSchedule != null)
+        {
+            existingSchedule.Date = schedule.Date;
+            existingSchedule.Time = schedule.Time;
+            existingSchedule.TypeTrainings = schedule.TypeTrainings;
+
+            var existingTrainer = await _context.Trainers.FindAsync(new object[] { schedule.Trainer.Id }, cancellationToken);
+            if (existingTrainer != null)
+            {
+                existingSchedule.Trainer = existingTrainer;
+            }
+            else
+            {
+                _context.Attach(schedule.Trainer);
+                existingSchedule.Trainer = schedule.Trainer;
+            }
+
+            existingSchedule.Clients.Clear();
+
+            foreach(var client in schedule.Clients)
+            {
+                var existingClient = await _context.Clients.FindAsync(new object[] { client.Id }, cancellationToken);
+                if (existingClient != null)
+                {
+                    existingSchedule.Clients.Add(existingClient);
+                }
+                else
+                {
+                    _context.Attach(client);
+                    existingSchedule.Clients.Add(client);
+                }
+            }
+        }
+        else
+        {
+            var existingTrainer = await _context.Trainers.FindAsync(new object[] { schedule.Trainer.Id }, cancellationToken);
+            if (existingTrainer != null)
+            {
+                schedule.Trainer = existingTrainer;
+            }
+            else
+            {
+                _context.Attach(schedule.Trainer);
+            }
+
+            var updatedClients = new List<Client>();
+            foreach (var client in schedule.Clients)
+            {
+                var existingClient = await _context.Clients.FindAsync(new object[] { client.Id }, cancellationToken);
+                if (existingClient != null)
+                {
+                    updatedClients.Add(existingClient);
+                }
+                else
+                {
+                    _context.Attach(client);
+                    updatedClients.Add(client);
+                }
+            }
+            schedule.Clients = updatedClients;
+
+            _context.Schedules.Add(schedule);
+        }
+
         await _context.SaveChangesAsync(cancellationToken);
     }
 }
