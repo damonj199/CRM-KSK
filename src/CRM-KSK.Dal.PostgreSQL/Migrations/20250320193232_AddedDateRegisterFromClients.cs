04fb373ff2df289fe5f85f818fa38b39e993d47c
Dmitriy Bilitsa <dbilica19@gmail.com>
2025-03-20 22:44:35 +0300

    очистил базу, сделал тестовую БД и добавил дату регистрации

---
+++ src/CRM-KSK.Dal.PostgreSQL/Migrations/20250320193232_AddedDateRegisterFromClients.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM_KSK.Dal.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDateRegisterFromClients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dateRegister",
+                table: "clients",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dateRegister",
+                table: "clients");
+        }
+    }
+}
