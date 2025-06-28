db516798a65a6b0eec435868c0ce0640414db9b8
Dmitriy Bilitsa <dbilica19@gmail.com>
2025-06-28 23:58:29 +0300

    последние изменения

--- src/CRM-KSK.Core/Entities/Horse.cs
+++ src/CRM-KSK.Core/Entities/Horse.cs
@@ -6,13 +6,4 @@ public sealed class Horse
     public string Name { get; set; } = string.Empty;
     public int RowNumber { get; set; }
     public DateOnly StartWeek { get; set; }
-
-    //private static DateOnly GetCurrentWeekMonday()
-    //{
-    //    DateTime today = DateTime.Today;
-
-    //    int diff = (7 + (today.DayOfWeek - DayOfWeek.Monday)) % 7;
-    //    DateTime monday = today.AddDays(-diff);
-    //    return DateOnly.FromDateTime(monday);
-    //}
 }
